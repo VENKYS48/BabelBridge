5bbb9e0ab6d5ea5b011d2de244607b7249e314b3
VENKYS48 <90025132+VENKYS48@users.noreply.github.com>
2024-01-03 15:03:31 +0000

    adding the base files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const port = process.env.PORT || 5000
+const app = express
+
+app.get("/", (req,res) => {
+    res.send("Server is running !")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
